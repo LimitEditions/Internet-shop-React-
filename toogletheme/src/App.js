386d9593ffd56f3e8662c733afe3c0c485e5f745
Aнастасия <007badgerl@gmail.com>
2024-04-18 17:24:54 +0300

    Домашнее задание 5

---
+++ toogletheme/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ThemeToggle from './ThemeToggle';
+
+function App() {
+  return (
+    <div className="App">
+      <ThemeToggle />
+    </div>
+  );
+}
+
+export default App;
